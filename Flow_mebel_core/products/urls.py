a10e1cdb283c7aee33871fe7ee9cf548502c492f
Gleb <asd>
2020-11-27 15:29:50 +0600

    gleb url

--- Flow_mebel_core/products/urls.py
+++ Flow_mebel_core/products/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.get_all, name = 'products'),
-    path('detail/<int:id>', views.detail_prod, name = 'details')
+    path('detail/<int:id>', views.detail_prod, name = 'details'),
 ]
