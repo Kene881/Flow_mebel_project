ca3f1390971feecd9cceaf16d1275523797f05c9
Gleb <asd>
2020-11-27 15:37:13 +0600

    gleb i dont know

--- Flow_mebel_core/products/admin.py
+++ Flow_mebel_core/products/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Product
+from .models import Order
+from .models import Image_for_prod
 
 # Register your models here.
 @admin.register(Product)
@@ -7,3 +9,15 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'cost')
     search_fields = ['title']
     list_filter = ('title',)
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'phone_number', 'product', 'size', 'material')
+    search_fields = ['first_name']
+    list_filter = ('first_name',)
+
+@admin.register(Image_for_prod)
+class Image_for_prodAdmin(admin.ModelAdmin):
+    list_display = ('product', 'image')
+    search_fields = ['product']
+    list_filter = ('product',)
