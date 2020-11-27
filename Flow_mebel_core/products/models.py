82cb944c159058fc55c2d21722a308d68bff437a
Kene <kenekene753@gmail.com>
2020-11-27 14:54:57 +0600

    prod image change

--- Flow_mebel_core/products/models.py
+++ Flow_mebel_core/products/models.py
@@ -4,6 +4,7 @@ class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     cost = models.CharField(max_length=50)
+    image = models.ImageField(null=True, blank=True)
     def __str__(self):
         return f'Product: {self.title}'
 
