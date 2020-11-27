01ca1180a5ce7ce7d5c7a8f74bbce7e30bc1d56e
Kene <kenekene753@gmail.com>
2020-11-27 14:39:45 +0600

    Kenesary change prod

--- Flow_mebel_core/products/models.py
+++ Flow_mebel_core/products/models.py
@@ -4,7 +4,9 @@ class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     cost = models.CharField(max_length=50)
-    image = models.ImageField(null=True, blank=True)
-    
     def __str__(self):
         return f'Product: {self.title}'
+
+class Image_for_prod(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
