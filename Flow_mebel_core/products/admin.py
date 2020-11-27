e6a34a2f11dfe7f4ffaca2d677efb98326807303
Gleb <asd>
2020-11-27 14:22:20 +0600

    gleb admin dorm url

--- Flow_mebel_core/products/admin.py
+++ Flow_mebel_core/products/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import Product
-from .models import Registration
 
 # Register your models here.
 @admin.register(Product)
@@ -8,9 +7,3 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'cost')
     search_fields = ['title']
     list_filter = ('title',)
-
-@admin.register(Registration)
-class RegistrationAdmin(admin.ModelAdmin):
-    list_display = ('name', 'phone_number', 'email')
-    search_fields = ['name']
-    list_filter = ('name',)
