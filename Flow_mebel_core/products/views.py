c8a51cfad9610ea4a6466f2732d782309db3e642
Gleb <asd>
2020-11-27 11:19:53 +0600

    gleb products views model

--- Flow_mebel_core/products/views.py
+++ Flow_mebel_core/products/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def get_all(request):
+    products = Product.objects.all()
+    context = {
+        'items': products
+    }
+    return render(request=request, template_name='products/products.html', context=context)
