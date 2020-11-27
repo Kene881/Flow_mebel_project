71c0024a851c52db4001b4786e99c8e01274e166
Batyrkhan <Sharipbay.batyr@gmail.com>
2020-11-27 11:08:12 +0600

    Batyrkhan auth init

--- Flow_mebel_core/auth_user/views.py
+++ Flow_mebel_core/auth_user/views.py
@@ -1,18 +1,19 @@
 from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
 from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
 from auth_user.forms import UserForm
+
 
 def login_page(request):
     if request.user.is_authenticated:
-        return redirect('menu')
+        return redirect('main_page')
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         user_auth = authenticate(request, username=username, password=password)
         if user_auth:
             request.session['id_user'] = user_auth.id
-            print(request.session['id_user'])
             login(request, user_auth)
             return redirect('menu')
     return render(request=request, template_name='auth/login.html')
@@ -35,6 +36,7 @@ def register_page(request):
     return render(request, 'auth/register.html', context)
 
 
+@login_required(login_url='/auth/login')
 def logout_page(request):
     logout(request)
     return redirect('login')
